6e43d24478cb5520117f1aec2eb305010c5869d5
TreetheFirst <126304980+TreetheFirst@users.noreply.github.com>
2023-03-28 09:27:57 -0500

    Delete Frogger_remake directory

--- Frogger_remake/Enemy.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-const speed = 50
-
-func _process(delta):
-	position.x += speed*delta
-
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
--- Frogger_remake/Log.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-const speed = 100
-
-func _process(delta):
-	position.x += speed*delta
