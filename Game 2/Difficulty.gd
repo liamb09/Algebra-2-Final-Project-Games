b2718ed2a061adcc02549fbb417d0b481653c63d
Rohan Kummaraguntla <69653360+RohanKummaraguntla@users.noreply.github.com>
2023-04-03 20:36:00 -0500

    Add files via upload

--- Game 2/Difficulty.gd
+++ Game 2/Difficulty.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name Difficulty
 
 func _ready():
 	$Beginner.grab_focus()
@@ -8,8 +9,14 @@ func _on_Experienced_pressed():
 
 
 func _on_Beginner_pressed():
-	pass # Replace with function body.
-
+	var pressed = true
+	get_tree().change_scene("res://Level.tscn")
+	return pressed
 
 func _on_Back_pressed():
 	get_tree().change_scene("res://PowerUps/Intro.tscn")
+
+func speed_change(pressed):
+	if pressed == true:
+		var speed = 1000
+
--- Game 2/PowerUps/Intro.gd
+++ Game 2/PowerUps/Intro.gd
@@ -10,7 +10,6 @@ func _on_Button_pressed():
 func _on_Button3_pressed():
 	get_tree().quit()
 
-
 func _on_Button4_pressed():
 	get_tree().change_scene("res://PowerUps/Tutorial Choice.tscn")
 
